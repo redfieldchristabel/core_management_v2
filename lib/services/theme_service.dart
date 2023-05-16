f95f49643d15685962041ac22e0823b3bfcdf1ce
imano00 <nurimanjamal00@gmail.com>
2023-05-16 16:00:31 +0800

    add documentation

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:core_management_v2/core_management_v2.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,24 +8,64 @@ class ThemeService {
     return themeData.cardTheme.copyWith(
       color: darkMode
           ? ElevationOverlay.applySurfaceTint(
-        HexColor.fromHex('#121212'),
-        Colors.white,
-        1,
-      )
+              HexColor.fromHex('#121212'),
+              Colors.white,
+              1,
+            )
           : HexColor.fromHex('#F8FAFB'),
     );
   }
 
+  /// Creates an `ElevatedButtonThemeData` object based on the specified parameters.
+  ///
+  /// This function takes a boolean parameter `darkMode` to determine the color scheme
+  /// of the button. If `darkMode` is `true`, it creates a button theme with dark colors.
+  /// Otherwise, it creates a button theme with light colors.
+  ///
+  /// The `ElevatedButtonThemeData` object represents the visual properties and
+  /// behavior of an elevated button when used within a theme.
+  ///
+  /// The `style` property of the `ElevatedButtonThemeData` is set using
+  /// `ElevatedButton.styleFrom`, which allows customizing various visual properties
+  /// of the button.
+  ///
+  /// The properties set by `ElevatedButton.styleFrom` in this function include:
+  ///
+  /// - `elevation`: The z-coordinate at which to place the button's surface relative
+  ///   to its parent.
+  ///
+  /// - `enableFeedback`: A boolean indicating whether the button should provide
+  ///   acoustic and haptic feedback upon interaction.
+  ///
+  /// - `shadowColor`: The color of the button's shadow.
+  ///
+  /// - `foregroundColor`: The color used for the button's text and icon.
+  ///
+  /// - `textStyle`: The style of the button's text.
+  ///
+  /// - `padding`: The padding around the button's content.
+  ///
+  /// - `backgroundColor`: The color of the button's background.
+  ///
+  /// - `shape`: The shape of the button's border.
+  ///
+  /// The returned `ElevatedButtonThemeData` object encapsulates these properties
+  /// and can be used to define the theme of elevated buttons within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// ElevatedButtonThemeData myButtonTheme = _elevatedButtonTheme(true);
+  /// ```
   ElevatedButtonThemeData _elevatedButtonTheme(bool darkMode) {
     return ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         elevation: 2,
         enableFeedback: true,
-        shadowColor: Color(0xFF00B6F0),
+        shadowColor: const Color(0xFF00B6F0),
         foregroundColor: Colors.white,
         textStyle: const TextStyle(color: Color(0xFFFFFFFF)),
         padding: const EdgeInsets.all(15),
-        backgroundColor: Color(0xFF00B6F0),
+        backgroundColor: const Color(0xFF00B6F0),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
         ),
@@ -35,29 +73,82 @@ class ThemeService {
     );
   }
 
+  /// Creates an `AppBarTheme` object based on the specified parameters.
+  ///
+  /// This function takes a `ThemeData` object `themeData` and a boolean parameter
+  /// `darkMode` to determine the appearance of the app bar. If `darkMode` is `true`,
+  /// it creates an app bar theme with dark colors. Otherwise, it creates an app bar
+  /// theme with light colors.
+  ///
+  /// The `AppBarTheme` object represents the visual properties and behavior of an
+  /// app bar when used within a theme.
+  ///
+  /// The `backgroundColor` property of the `AppBarTheme` is set to a constant color
+  /// value `Color(0xFF00B6F0)`, which represents the background color of the app bar.
+  ///
+  /// The `titleTextStyle` property is set using `themeData.textTheme.titleLarge`, which
+  /// represents the text style for the app bar's title. It is derived from the `themeData`
+  /// object provided as a parameter.
+  ///
+  /// The `systemOverlayStyle` property is set to a `SystemUiOverlayStyle` object,
+  /// which allows customizing the appearance of system overlays like the status bar
+  /// and navigation bar. The `statusBarIconBrightness` property of `SystemUiOverlayStyle`
+  /// is set to `Brightness.light` if `darkMode` is `true`, otherwise it is set to
+  /// `Brightness.dark`. The `statusBarBrightness` property is set to `themeData.brightness`,
+  /// which represents the brightness of the overall app theme. The `systemNavigationBarColor`
+  /// property is set to `HexColor.fromHex('#121212')` if `darkMode` is `true`, otherwise
+  /// it is set to `Color(0xFFFFFFFF)`.
+  ///
+  /// The returned `AppBarTheme` object encapsulates these properties and can be used
+  /// to define the theme of app bars within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// AppBarTheme myAppBarTheme = _appBarTheme(myThemeData, true);
+  /// ```
   AppBarTheme _appBarTheme(ThemeData themeData, bool darkMode) {
     return themeData.appBarTheme.copyWith(
-      backgroundColor: Color(0xFF00B6F0),
+      backgroundColor: const Color(0xFF00B6F0),
       titleTextStyle: themeData.textTheme.titleLarge,
       systemOverlayStyle: SystemUiOverlayStyle(
-        // systemStatusBarContrastEnforced: true,
         statusBarIconBrightness: darkMode ? Brightness.light : Brightness.dark,
         statusBarBrightness: themeData.brightness,
         systemNavigationBarColor:
-        darkMode ? HexColor.fromHex('#121212') : Color(0xFFFFFFFF),
+            darkMode ? HexColor.fromHex('#121212') : const Color(0xFFFFFFFF),
       ),
     );
   }
 
+  /// Creates a `BottomSheetThemeData` object based on the specified parameters.
+  ///
+  /// This function takes a `ThemeData` object `themeData` and a boolean parameter
+  /// `darkMode` to determine the appearance of the bottom sheet. If `darkMode` is `true`,
+  /// it creates a bottom sheet theme with dark colors. Otherwise, it creates a bottom sheet
+  /// theme with light colors.
+  ///
+  /// The `BottomSheetThemeData` object represents the visual properties and behavior of a
+  /// bottom sheet when used within a theme.
+  ///
+  /// The `shape` property of the `BottomSheetThemeData` is set to a constant `RoundedRectangleBorder`
+  /// with a vertical `BorderRadius` of `Radius.circular(20)`. This gives the bottom sheet rounded
+  /// corners at the top.
+  ///
+  /// The `clipBehavior` property is set to `Clip.antiAliasWithSaveLayer`, which determines how
+  /// the content of the bottom sheet is clipped to its shape. `Clip.antiAliasWithSaveLayer` ensures
+  /// that the content is clipped with antialiasing to provide smooth edges.
+  ///
+  /// The returned `BottomSheetThemeData` object encapsulates these properties and can be used
+  /// to define the theme of bottom sheets within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// BottomSheetThemeData myBottomSheetTheme = _bottomSheetThemeData(myThemeData, true);
+  /// ```
   BottomSheetThemeData _bottomSheetThemeData(
-      ThemeData themeData,
-      bool darkMode,
-      ) {
+    ThemeData themeData,
+    bool darkMode,
+  ) {
     return themeData.bottomSheetTheme.copyWith(
-      // transitionAnimationController: AnimationController(
-      //   vsync: this,
-      //   duration: Duration(milliseconds: 400),
-      // ),
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
@@ -65,10 +156,50 @@ class ThemeService {
     );
   }
 
+  /// Creates an `InputDecorationTheme` object based on the specified parameters.
+  ///
+  /// This function takes a `ThemeData` object `themeData` and a boolean parameter `darkMode`
+  /// to determine the appearance of the input decoration. If `darkMode` is `true`, it creates
+  /// an input decoration theme with dark colors. Otherwise, it creates an input decoration theme
+  /// with light colors.
+  ///
+  /// The `InputDecorationTheme` object represents the visual properties and behavior of an input
+  /// decoration when used within a theme.
+  ///
+  /// The `contentPadding` property of the `InputDecorationTheme` is set to `EdgeInsets.all(3)`
+  /// with a left padding of `9`. This provides padding around the input content.
+  ///
+  /// The `labelStyle` property is set to a `TextStyle` object with a `fontWeight` of `FontWeight.w600`,
+  /// a `fontSize` of `16`, a `letterSpacing` of `0.2`, and a color of `HexColor.fromHex('#B9BABC')`.
+  /// This defines the style for the input label text.
+  ///
+  /// The `helperStyle` property is set to a `TextStyle` object with a `fontWeight` of `FontWeight.bold`,
+  /// a `fontSize` of `16`, and a color of `HexColor.fromHex('#B9BABC')`. This defines the style for
+  /// the input helper text.
+  ///
+  /// The `fillColor` property is set to `null` if `darkMode` is `true`, otherwise it is set to
+  /// `HexColor.fromHex('#F8FAFB')`. This determines the background color of the input field.
+  ///
+  /// The `prefixIconColor` and `suffixIconColor` properties are set to `themeData.colorScheme.primary`,
+  /// which represents the color of the prefix and suffix icons.
+  ///
+  /// The `filled` property is set to `true` to enable the input field to be filled with a background color.
+  ///
+  /// The `border` property is set to a `OutlineInputBorder` object with `BorderSide.none` for the side
+  /// and `BorderRadius.circular(13.0)` for all four corners. This defines the border appearance of the
+  /// input field.
+  ///
+  /// The returned `InputDecorationTheme` object encapsulates these properties and can be used to define
+  /// the theme of input decorations within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// InputDecorationTheme myInputDecorationTheme = _inputDecorationTheme(myThemeData, true);
+  /// ```
   InputDecorationTheme _inputDecorationTheme(
-      ThemeData themeData,
-      bool darkMode,
-      ) {
+    ThemeData themeData,
+    bool darkMode,
+  ) {
     return themeData.inputDecorationTheme.copyWith(
       contentPadding: const EdgeInsets.all(3).copyWith(left: 9),
       labelStyle: TextStyle(
@@ -99,9 +230,50 @@ class ThemeService {
     );
   }
 
+  /// Creates a `TabBarTheme` object based on the specified parameters.
+  ///
+  /// This function takes a `ThemeData` object `themeData` and a boolean parameter `darkMode`
+  /// to determine the appearance of the tab bar. If `darkMode` is `true`, it creates a tab bar
+  /// theme with dark colors. Otherwise, it creates a tab bar theme with light colors.
+  ///
+  /// The `TabBarTheme` object represents the visual properties and behavior of a tab bar when used
+  /// within a theme.
+  ///
+  /// The `labelColor` property of the `TabBarTheme` is set to `Color(0xFF3A5160)`, which represents
+  /// the color of the selected tab label.
+  ///
+  /// The `unselectedLabelColor` property is set to `Colors.white70`, which represents the color of
+  /// the unselected tab labels.
+  ///
+  /// The `labelStyle` property is set to a `TextStyle` object with a `fontWeight` of `FontWeight.w400`,
+  /// which defines the style for the selected tab label text.
+  ///
+  /// The `unselectedLabelStyle` property is set to a `TextStyle` object with a `fontStyle` of `FontStyle.italic`,
+  /// which defines the style for the unselected tab label text.
+  ///
+  /// The `overlayColor` property is set to a `MaterialStateColor` object that resolves the overlay color
+  /// based on the current material state. If the state contains `MaterialState.pressed`, the overlay color
+  /// is `Colors.blue`. If the state contains `MaterialState.focused`, the overlay color is `Colors.orange`.
+  /// If the state contains `MaterialState.hovered`, the overlay color is `Color(0xFF00B6F0)`. For any other
+  /// state, the overlay color is `Colors.transparent`.
+  ///
+  /// The `indicator` property is set to a `BoxDecoration` object that defines the appearance of the tab bar's
+  /// indicator. It has a background color of `HexColor.fromHex('#F8FAFB')`, a border with color `Colors.white`,
+  /// and a `BorderRadius` of `BorderRadius.all(Radius.circular(16.0))`.
+  ///
+  /// The `indicatorSize` property is set to `TabBarIndicatorSize.tab`, which indicates that the indicator's
+  /// size should be based on the tab's dimensions.
+  ///
+  /// The returned `TabBarTheme` object encapsulates these properties and can be used to define the theme of
+  /// tab bars within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// TabBarTheme myTabBarTheme = _tabBarTheme(myThemeData, true);
+  /// ```
   TabBarTheme _tabBarTheme(ThemeData themeData, bool darkMode) {
     return themeData.tabBarTheme.copyWith(
-      labelColor: Color(0xFF3A5160),
+      labelColor: const Color(0xFF3A5160),
       unselectedLabelColor: Colors.white70,
       labelStyle: const TextStyle(fontWeight: FontWeight.w400),
       unselectedLabelStyle: const TextStyle(fontStyle: FontStyle.italic),
@@ -112,7 +284,7 @@ class ThemeService {
         if (states.contains(MaterialState.focused)) {
           return Colors.orange;
         } else if (states.contains(MaterialState.hovered)) {
-          return Color(0xFF00B6F0);
+          return const Color(0xFF00B6F0);
         }
 
         return Colors.transparent;
@@ -128,10 +300,43 @@ class ThemeService {
     );
   }
 
+  /// Creates a `ChipThemeData` object based on the specified parameters.
+  ///
+  /// This function takes a `ThemeData` object `themeData` and a boolean parameter `darkMode`
+  /// to determine the appearance of the chips. If `darkMode` is `true`, it creates a chip theme
+  /// with dark colors. Otherwise, it creates a chip theme with light colors.
+  ///
+  /// The `ChipThemeData` object represents the visual properties and behavior of chips when used
+  /// within a theme.
+  ///
+  /// The `backgroundColor` property of the `ChipThemeData` is set to the result of `ElevationOverlay.applySurfaceTint`
+  /// method. It applies a surface tint to the background color based on the surface color and the specified
+  /// elevation value. The background color is `darkMode ? HexColor.fromHex('#121212') : Color(0xFFFFFFFF)`
+  /// and the surface color is `Colors.white`. The elevation value is `2`.
+  ///
+  /// The `side` property is set to a `BorderSide` object with a color of `Color(0xFFEDF0F2)`, which
+  /// defines the color of the chip's side border.
+  ///
+  /// The `padding` property is set to `EdgeInsets.all(5)`, which defines the padding around the chip's content.
+  ///
+  /// The `shadowColor` property is set to `Colors.black`, which defines the color of the chip's shadow.
+  ///
+  /// The `elevation` property is set to `5`, which determines the elevation of the chip.
+  ///
+  /// The `labelStyle` property is set to a `TextStyle` object with a `fontSize` of `10`, a color of
+  /// `Color(0xFF00B6F0)`, and a `fontWeight` of `FontWeight.w500`. This defines the style for the chip's label text.
+  ///
+  /// The returned `ChipThemeData` object encapsulates these properties and can be used to define the theme of
+  /// chips within a theme.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// ChipThemeData myChipTheme = _chipThemeData(myThemeData, true);
+  /// ```
   ChipThemeData _chipThemeData(ThemeData themeData, bool darkMode) {
     return themeData.chipTheme.copyWith(
       backgroundColor: ElevationOverlay.applySurfaceTint(
-        darkMode ? HexColor.fromHex('#121212') : Color(0xFFFFFFFF),
+        darkMode ? HexColor.fromHex('#121212') : const Color(0xFFFFFFFF),
         Colors.white,
         2,
       ),
@@ -148,11 +353,11 @@ class ThemeService {
   }
 
   TextTheme _textTheme(
-      ThemeData themeData,
-      Color primaryColor,
-      bool darkMode,
-      bool darkColor,
-      ) {
+    ThemeData themeData,
+    Color primaryColor,
+    bool darkMode,
+    bool darkColor,
+  ) {
     return themeData.textTheme.copyWith(
       displayLarge: _displayLarge(themeData, darkMode),
       displayMedium: _displayMedium(themeData, darkMode),
@@ -183,7 +388,7 @@ class ThemeService {
 
   TextStyle _bodyLarge(ThemeData themeData, bool darkMode) {
     return themeData.textTheme.bodyLarge!.copyWith(
-      color: darkMode ? Colors.white : Color(0xFF17262A),
+      color: darkMode ? Colors.white : const Color(0xFF17262A),
       fontSize: 12,
       // fontFamily: "OpenSansBold",
     );
@@ -194,13 +399,13 @@ class ThemeService {
       fontWeight: FontWeight.w600,
       fontSize: 18,
       letterSpacing: 0.27,
-      color: Color(0xFF00B6F0),
+      color: const Color(0xFF00B6F0),
     );
   }
 
   TextStyle _titleMedium(ThemeData themeData, bool darkMode) {
     return themeData.textTheme.titleMedium!.copyWith(
-      color: darkMode ? Color(0xFFFFFFFF) : Color(0xFF3A5160),
+      color: darkMode ? const Color(0xFFFFFFFF) : const Color(0xFF3A5160),
       fontWeight: FontWeight.w200,
       fontSize: 12,
       letterSpacing: 0.21,
@@ -209,11 +414,11 @@ class ThemeService {
   }
 
   TextStyle _titleLarge(
-      ThemeData themeData,
-      bool darkMode,
-      ) {
+    ThemeData themeData,
+    bool darkMode,
+  ) {
     return themeData.textTheme.titleLarge!.copyWith(
-      color: darkMode ? Color(0xFFFFFFFF) : Colors.black,
+      color: darkMode ? const Color(0xFFFFFFFF) : Colors.black,
       // fontFamily: "OpenSansBold",
     );
   }
@@ -232,7 +437,7 @@ class ThemeService {
       fontWeight: FontWeight.w600,
       fontSize: 16,
       letterSpacing: 0.27,
-      color: darkMode ? HexColor.fromHex('#B9BABC') : Color(0xFF17262A),
+      color: darkMode ? HexColor.fromHex('#B9BABC') : const Color(0xFF17262A),
     );
   }
 
@@ -241,7 +446,7 @@ class ThemeService {
       fontWeight: FontWeight.w400,
       fontSize: 14,
       letterSpacing: 0.2,
-      color: darkMode ? Color(0xFFFEFEFE) : Color(0xFF3A5160),
+      color: darkMode ? const Color(0xFFFEFEFE) : const Color(0xFF3A5160),
     );
   }
 
@@ -250,7 +455,7 @@ class ThemeService {
       fontWeight: FontWeight.w700,
       fontSize: 22,
       letterSpacing: 0.27,
-      color: darkMode ? const Color(0xFFFFFFFF) : Color(0xFF253840),
+      color: darkMode ? const Color(0xFFFFFFFF) : const Color(0xFF253840),
     );
   }
 
@@ -270,16 +475,16 @@ class ThemeService {
 
     final hsl = HSLColor.fromColor(color);
     final hslLight =
-    hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
 
     return hslLight.toColor();
   }
 
   ///function to create theme data by primary color
   ThemeData createThemeDataByPrimaryColour(
-      ThemeData themeData,
-      Color primaryColor,
-      ) {
+    ThemeData themeData,
+    Color primaryColor,
+  ) {
     bool darkMode = themeData.brightness == Brightness.dark;
     bool darkColor =
         ThemeData.estimateBrightnessForColor(primaryColor) == Brightness.dark;
@@ -304,13 +509,12 @@ class ThemeService {
         color: darkMode ? const Color(0xFF253840) : const Color(0xFFFEFEFE),
       ),
       scaffoldBackgroundColor:
-      darkMode ? HexColor.fromHex('#121212') : const Color(0xFFFEFEFE),
+          darkMode ? HexColor.fromHex('#121212') : const Color(0xFFFEFEFE),
       shadowColor: HexColor.fromHex('#121212').withOpacity(0.2),
       appBarTheme: _appBarTheme(themeData, darkMode),
       chipTheme: _chipThemeData(themeData, darkMode),
       listTileTheme: themeData.listTileTheme.copyWith(
-        iconColor:
-        darkMode ? const Color(0xFFFEFEFE) : const Color(0xFF00B6F0),
+        iconColor: darkMode ? const Color(0xFFFEFEFE) : const Color(0xFF00B6F0),
       ),
       bottomSheetTheme: _bottomSheetThemeData(themeData, darkMode),
       dividerTheme: DividerThemeData(
@@ -324,5 +528,4 @@ class ThemeService {
       textTheme: _textTheme(themeData, primaryColor, darkMode, darkColor),
     );
   }
-
 }
