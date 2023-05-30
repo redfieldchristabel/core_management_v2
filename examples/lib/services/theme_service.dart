cd6c414e2fcf734ebed0ef4212923da9e7eb3100
redfield <redfieldchristabel@gmail.com>
2023-05-30 10:29:49 +0800

    add package to example folder

---
+++ examples/lib/services/theme_service.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:core_management_v2/core_management_v2.dart';
+import 'package:flutter/material.dart';
+
+class ThemeService extends BaseThemeService {
+  @override
+  Color get colorSeed => Colors.purple;
+
+
+  @override
+  AppBarTheme appBarTheme(ThemeData themeData, bool darkMode) {
+    // TODO: implement appBarTheme
+    return super.appBarTheme(themeData, darkMode);
+  }
+
+
+
+}
