04c0d616d49633b9cea0f0b00e0a1db38f1281b5
redfield <redfieldchristabel@gmail.com>
2023-11-02 17:58:37 +0800

    fix example

--- examples/lib/screens/loading_screen.dart
+++ examples/lib/screens/loading_screen.dart
@@ -1,4 +1,4 @@
-import 'package:examples/widgets/loading_animation.dart';
+import 'package:core_management_v2/core_management_v2.dart';
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatelessWidget {
