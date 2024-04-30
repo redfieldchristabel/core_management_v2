8d15749fa9c4040e56e152edb312df38ec94cba8
redfieldchristabel <redfieldchristabel@gmail.com>
2024-04-30 14:44:36 +0800

    add new json converter

---
+++ lib/models/json_converter/time_of_day_to_string.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class TimeOfDayConverter implements JsonConverter<TimeOfDay, int> {
+  final bool isMicroseconds;
+
+  TimeOfDayConverter({this.isMicroseconds = false});
+
+  @override
+  TimeOfDay fromJson(int json) {
+    return TimeOfDay.fromDateTime(DateTime.fromMicrosecondsSinceEpoch(
+        json * (isMicroseconds ? 1 : 1000)));
+  }
+
+  @override
+  int toJson(TimeOfDay object) {
+    return (object.hour * object.minute) * 60 * (isMicroseconds ? 1000 : 1);
+  }
+}
