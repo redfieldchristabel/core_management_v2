a43bf35afc92cf65129b579b4c7c097cfb230ba0
redfieldchristabel <epoolr88@gmail.com>
2024-06-26 09:30:10 +0800

    use overlay portal to get the child context

--- lib/widgets/loading_chip_wrapper.dart
+++ lib/widgets/loading_chip_wrapper.dart
@@ -12,35 +12,38 @@ class LoadingChipWrapper extends StatefulWidget {
   State<LoadingChipWrapper> createState() => _LoadingChipWrapperState();
 }
 
-class _LoadingChipWrapperState extends State<LoadingChipWrapper> {
+class _LoadingChipWrapperState extends State<LoadingChipWrapper>
+    with TaskQueueMixin {
   final LoadingStateChipController _loadingStateChipController =
       LoadingStateChipController();
+  final OverlayPortalController _controller = OverlayPortalController();
 
   @override
   void initState() {
     BaseFrameworkService.instance.initializeLoadingController(
         chipController: _loadingStateChipController);
+
+    enqueueTask(() => _controller.show());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        widget.child,
-        IgnorePointer(
-          child: Material(
-            color: Colors.transparent,
-            child: SafeArea(
-              child: Align(
-                alignment: Alignment.topRight,
-                child: LoadingStateChip(
-                    loadingStateChipController: _loadingStateChipController),
-              ),
+    return OverlayPortal(
+      controller: _controller,
+      overlayChildBuilder: (context) => IgnorePointer(
+        child: Material(
+          color: Colors.transparent,
+          child: SafeArea(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: LoadingStateChip(
+                  loadingStateChipController: _loadingStateChipController),
             ),
           ),
         ),
-      ],
+      ),
+      child: widget.child,
     );
   }
 }
